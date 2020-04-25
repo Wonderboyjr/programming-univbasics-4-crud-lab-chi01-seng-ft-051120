bf619eb8330d8f8e0c629daa724f95915bfa5f1d
Jonathan Reid <iamjonathanreid@gmail.com>
2020-04-25 22:12:58 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -22,11 +22,12 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  
+  game_icons = ["mario", "luigi"]
   game_icons.shift("sora")
 end
 
 def retrieve_element_from_index(array, index_number)
+  
   game_icons[1]
 end
 
