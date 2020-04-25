3259e094b3d4889dc988207942fbb304daa7045d
Jonathan Reid <iamjonathanreid@gmail.com>
2020-04-25 22:12:20 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,11 +7,12 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  game_icons 
+  game_icons = ["mario", "luigi"]
   game_icons << "kratos"
 end
 
 def add_element_to_start_of_array(array, element)
+  
   game_icons.unshift("peach")
 end
 
