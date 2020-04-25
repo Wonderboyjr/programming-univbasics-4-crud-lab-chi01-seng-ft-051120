de4a8ce67aa4445ae6140a633cd60c97145b60e8
Jonathan Reid <iamjonathanreid@gmail.com>
2020-04-25 22:13:03 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -27,11 +27,12 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  game_icons = ["mario", "luigi"]
   game_icons[1]
 end
 
 def retrieve_first_element_from_array(array)
+  
   game_icons[0]
 end
 
