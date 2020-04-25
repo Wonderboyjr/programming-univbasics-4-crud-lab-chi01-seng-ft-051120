1c9fca6621519ba256bbc0d807f76ba15dc9ea55
Jonathan Reid <iamjonathanreid@gmail.com>
2020-04-25 22:16:45 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -27,12 +27,11 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-  game_icons = ["mario", "luigi"]
-  game_icons[0]
+  game_icons.first
 end
 
 def retrieve_last_element_from_array(array)
-  game_icons[-1]
+  game_icons.last
 end
 
 def update_element_from_index(array, index_number, element)
