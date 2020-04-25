1983646f44ae24ef4cae9f2607013fd8d30e76b6
Jonathan Reid <iamjonathanreid@gmail.com>
2020-04-25 22:22:25 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -18,10 +18,12 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  game_icons.pop
+  game_icons = ["mario", "link", "sonic", "yoshi"]
+  game_icons.pop("ryu")
 end
 
 def remove_element_from_start_of_array(array)
+  
   game_icons.shift
 end
 
